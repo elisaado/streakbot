79f29dcb08caf328c4bfbb923c364764f11b1741
Eli Saado <eli5saado@gmail.com>
2020-08-06 23:50:57 +0200

    Add some command and event listeners

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const TelegramBot = require('node-telegram-bot-api');
+
 const config = require('./config.json');
 const streaks = require('./db');
 
+const bot = new TelegramBot(config.token, {polling: true});
+
+bot.onText(/^\/streak$/, (msg) => {
+  const resp = "";
+  const user = msg.from.id;
+
+
+  bot.sendMessage(msg.chat.id, resp, {reply_to_message_id: msg.message_id});
+});
+
+bot.onText(/^\/relapse$/, (msg) => {
+  const resp = "";
+  const user = msg.from.id;
+
+
+  bot.sendMessage(msg.chat.id, resp, {reply_to_message_id: msg.message_id});
+});
+
+bot.on('callback_query', (query) => {
+  const data = query.data;
+  const user = query.from.id;
+
+
+  bot.answerCallbackQuery(query.id, {})
+});
