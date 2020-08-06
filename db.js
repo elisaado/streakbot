cf8b689cbc8e615e94acd1947c974269ea2da8cb
Eli Saado <eli5saado@gmail.com>
2020-08-07 00:25:08 +0200

    Fix undefined db

--- db.js
+++ db.js
@@ -4,18 +4,17 @@ const assert = require('assert');
 // Connection URL
 const url = 'mongodb://localhost:27017';
  
-// Database Name
-const dbName = 'streakbot';
-
-let collection = {};
+let collection;
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
  
-  const db = client.db(dbName);
+  const db = client.db('streakbot');
   collection = db.collection('streaks');
 });
 
-module.exports = collection;
+module.exports = () => {
+  return collection;
+};
