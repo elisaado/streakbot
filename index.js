fc893feba146bfa0dec95225ffa9f3207f75429a
Eli Saado <eli5saado@gmail.com>
2020-08-07 01:26:38 +0200

    Add markdownv2, markdown escape and username to db

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const TelegramBot = require('node-telegram-bot-api');
+const escape = require('markdown-escape');
 
 const { daysBetween } = require('./util');
 const streaks = require('./db');
@@ -9,7 +10,7 @@ const config = require('./config.json');
 
 const bot = new TelegramBot(config.token, { polling: true });
 
-bot.onText(/^\/streak$/, async (msg) => {
+bot.onText(/^\/streak(@.+bot)?$/, async (msg) => {
   const user = msg.from.id;
   const streak = await streaks().findOne({ id: user });
 
@@ -20,15 +21,15 @@ bot.onText(/^\/streak$/, async (msg) => {
     const days = Math.floor(daysBetween(streak.start, new Date));
 
     resp = `
-Hey _${msg.from.first_name}_.
-🔥 Your streak is *${days} days* long.
+Hey _${escape(msg.from.first_name)}_\\.
+🔥 Your streak is *${days} days* long\\.
 
-❌ Use /relapse if you have relapsed.`;
+❌ Use /relapse if you have relapsed\\.`;
   } else {
     resp = `
-Hey _${msg.from.first_name}_, welcome to Streak bot.
+Hey _${escape(msg.from.first_name)}_, welcome to Streak bot\\.
 
-🏁 Tap start a new streak to start a new streak.`;
+🏁 Tap start a new streak to start a new streak\\.`;
 
     reply_markup = {
       inline_keyboard: [[{
@@ -38,21 +39,21 @@ Hey _${msg.from.first_name}_, welcome to Streak bot.
     }
   }
 
-  bot.sendMessage(msg.chat.id, resp, { reply_to_message_id: msg.message_id, parse_mode: "Markdown", reply_markup });
+  bot.sendMessage(msg.chat.id, resp, { reply_to_message_id: msg.message_id, parse_mode: "MarkdownV2", reply_markup });
 });
 
-bot.onText(/^\/relapse$/, async (msg) => {
+bot.onText(/^\/relapse(@.+bot)?$/, async (msg) => {
   let resp = "";
   const user = msg.from.id;
   const streak = await streaks().findOne({ id: user });
 
   if (!streak) {
     resp = `
-Hey _${msg.from.first_name}_, welcome to Streak bot.
+Hey _${escape(msg.from.first_name)}_, welcome to Streak bot\\.
     
-↪️ Use /start to start a new streak.`;
+↪️ Use /start to start a new streak\\.`;
 
-    return bot.sendMessage(msg.chat.id, resp, { reply_to_message_id: msg.message_id, parse_mode: "Markdown" });
+    return bot.sendMessage(msg.chat.id, resp, { reply_to_message_id: msg.message_id, parse_mode: "MarkdownV2" });
   }
 
   resp = `Are you sure you want to register a *relapse*?`;
@@ -69,7 +70,7 @@ Hey _${msg.from.first_name}_, welcome to Streak bot.
     ]]
   }
 
-  bot.sendMessage(msg.chat.id, resp, { reply_to_message_id: msg.message_id, parse_mode: "Markdown", reply_markup });
+  bot.sendMessage(msg.chat.id, resp, { reply_to_message_id: msg.message_id, parse_mode: "MarkdownV2", reply_markup });
 });
 
 bot.on('callback_query', async (query) => {
@@ -84,42 +85,42 @@ bot.on('callback_query', async (query) => {
     return bot.answerCallbackQuery(query.id, { text: "🚫 This button was not meant for you" })
   }
 
+  let username = query.from.username;
+  if (!username) username = `[${query.from.first_name}](tg://user?id=${user})`;
+
   switch (command) {
     case 'start': {
       const resp = `
-I started a new streak for you.
+I started a new streak for you\\.
 
-🍀 Good luck, _${query.from.first_name}_.`;
+🍀 Good luck, _${escape(query.from.first_name)}_\\.`;
 
-      await streaks().insertOne({ id: user, start: (new Date).getTime() });
-      bot.editMessageText(resp, { chat_id: msg.chat.id, message_id: msg.message_id, parse_mode: 'Markdown' })
+      await streaks().insertOne({ id: user, username, start: (new Date).getTime() });
+      bot.editMessageText(resp, { chat_id: msg.chat.id, message_id: msg.message_id, parse_mode: 'MarkdownV2' })
       break;
     }
     case 'relapse': {
       const streak = await streaks().findOne({ id: user });
       const days = Math.floor(daysBetween(streak.start, new Date));
 
-      await streaks().insertOne({ id: user, start: (new Date).getTime() });
+      await streaks().insertOne({ id: user, username, start: (new Date).getTime() });
 
       const resp = `
-🗑 Sad to see your streak of *${days} days* go down the drain.
+🗑 Sad to see your streak of *${days} days* go down the drain\\.
 
 
-I started a new streak for you.
+I started a new streak for you\\.
 
-🍀 Good luck, _${query.from.first_name}_, you will need it.`;
+🍀 Good luck, _${escape(query.from.first_name)}_, you will need it\\.`;
       
       await streaks().updateOne({ id: user}, {$set: { id: user, start: (new Date).getTime() }});
-      bot.editMessageText(resp, { chat_id: msg.chat.id, message_id: msg.message_id, parse_mode: 'Markdown' })
+      bot.editMessageText(resp, { chat_id: msg.chat.id, message_id: msg.message_id, parse_mode: 'MarkdownV2' })
       break;
     }
     case 'cancel': {
-      await streaks().insertOne({ id: user, start: (new Date).getTime() });
-
-      const resp = `
-🆗 Cancelled.`;
+      const resp = `🆗 Cancelled\\.`;
       
-      bot.editMessageText(resp, { chat_id: msg.chat.id, message_id: msg.message_id, parse_mode: 'Markdown' })
+      bot.editMessageText(resp, { chat_id: msg.chat.id, message_id: msg.message_id, parse_mode: 'MarkdownV2' })
       break;
     }
     default:
