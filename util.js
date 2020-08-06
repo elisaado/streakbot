df2290f4243380fd3abd4378059e6d364ddfae02
Eli Saado <eli5saado@gmail.com>
2020-08-07 00:50:35 +0200

    Add dateutil

---
+++ util.js
@@ -0,0 +1,14 @@
+function treatAsUTC(date) {
+  var result = new Date(date);
+  result.setMinutes(result.getMinutes() - result.getTimezoneOffset());
+  return result;
+}
+
+function daysBetween(startDate, endDate) {
+  var millisecondsPerDay = 24 * 60 * 60 * 1000;
+  return (treatAsUTC(endDate) - treatAsUTC(startDate)) / millisecondsPerDay;
+}
+
+module.exports = {
+  daysBetween
+}
